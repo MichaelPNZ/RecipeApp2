60834e8f492a01ece348060fc5c7dd77ae5cc955
PozyalovMihail <pozyalov@gmail.com>
2023-12-01 14:05:28 +0300

    RA-06. Создал data class'ы для Category, Recipe, Ingredient. Экспортировал изображения категорий из Figma и сохранил в assets. Внутри фрагмента CategoriesListFragment создал список с объектами типа Category и проинициализировал его.

--- app/src/main/java/com/example/recipeapp/CategoriesListFragment.kt
+++ app/src/main/java/com/example/recipeapp/CategoriesListFragment.kt
@@ -7,9 +7,32 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.recipeapp.databinding.FragmentListCategoriesBinding
 
+data class Category(
+    val id: Int,
+    val title: String,
+    val description: String,
+    val imageUrl: String,
+)
+
+data class Ingredient(
+    val quantity: Double,
+    val unitOfMeasure: String,
+    val description: String,
+)
+
+data class Recipe(
+    val id: Int,
+    val title: String,
+    val ingredients: List<Ingredient>,
+    val method: List<String>,
+    val imageUrl: String,
+)
+
+
 class CategoriesListFragment: Fragment() {
 
     private lateinit var binding: FragmentListCategoriesBinding
+    private var categoryList = mutableListOf<Category>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -21,6 +44,16 @@ class CategoriesListFragment: Fragment() {
             container,
             false
         )
+
+        categoryList = mutableListOf(
+            Category(0, "Бургеры", "Рецепты всех популярных видов бургеров", "burger.png"),
+            Category(1, "Десерты", "Самые вкусные рецепты десертов специально для вас", "dessert.png"),
+            Category(2, "Пицца", "Пицца на любой вкус и цвет. Лучшая подборка для тебя", "pizza.png"),
+            Category(3, "Рыба", "Печеная, жареная, сушеная, любая рыба на твой вкус", "fish.png"),
+            Category(4, "Супы", "От классики до экзотики: мир в одной тарелке", "soup.png"),
+            Category(5, "Салаты", "Хрустящий калейдоскоп под соусом вдохновения", "salad.png"),
+        )
+
         return binding.root
     }
 
