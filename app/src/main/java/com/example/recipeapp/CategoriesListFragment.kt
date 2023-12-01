f3b742b81f557396f73a6c99907f124c64023e53
PozyalovMihail <pozyalov@gmail.com>
2023-12-01 21:46:30 +0300

    RA-06. Поправил картинки и вынес data class'ы в отдельные файлы..

--- app/src/main/java/com/example/recipeapp/CategoriesListFragment.kt
+++ app/src/main/java/com/example/recipeapp/CategoriesListFragment.kt
@@ -5,29 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.recipeapp.data.Category
 import com.example.recipeapp.databinding.FragmentListCategoriesBinding
-
-data class Category(
-    val id: Int,
-    val title: String,
-    val description: String,
-    val imageUrl: String,
-)
-
-data class Ingredient(
-    val quantity: Double,
-    val unitOfMeasure: String,
-    val description: String,
-)
-
-data class Recipe(
-    val id: Int,
-    val title: String,
-    val ingredients: List<Ingredient>,
-    val method: List<String>,
-    val imageUrl: String,
-)
-
 
 class CategoriesListFragment: Fragment() {
 
