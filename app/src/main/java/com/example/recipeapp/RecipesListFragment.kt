be86cad5a9a6d99ff54f94de5eba08af342baba2
PozyalovMihail <pozyalov@gmail.com>
2023-12-13 19:54:51 +0300

    RA-11 Реализовал экран со списком рецептов.

--- app/src/main/java/com/example/recipeapp/RecipesListFragment.kt
+++ app/src/main/java/com/example/recipeapp/RecipesListFragment.kt
@@ -1,9 +1,14 @@
 package com.example.recipeapp
 
+import android.annotation.SuppressLint
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.example.recipeapp.databinding.FragmentListRecipesBinding
 
@@ -28,13 +33,21 @@ class RecipesListFragment: Fragment() {
         return binding.root
     }
 
+    @SuppressLint("DiscouragedApi")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
        arguments?.let {
            categoryId = it.getInt(ARG_CATEGORY_ID)
            categoryName= it.getString(ARG_CATEGORY_NAME)
            categoryImageUrl = it.getString(ARG_CATEGORY_IMAGE_URL)
        }
+
+        val headerImageView: ImageView = view.findViewById(R.id.headerImageView)
+        val categoryNameTextView: TextView = view.findViewById(R.id.tvCategoryNameTextView)
+
+//        headerImageView.setImageDrawable(resources.assets.open(categoryImageUrl))
+        categoryNameTextView.text = categoryName
     }
 
 }
