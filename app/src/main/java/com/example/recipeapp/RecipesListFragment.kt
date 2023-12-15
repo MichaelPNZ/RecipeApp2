39f153e6ae96c2e81d9467cacc791738b193e9b3
PozyalovMihail <pozyalov@gmail.com>
2023-12-15 14:08:30 +0300

    RA-11

--- app/src/main/java/com/example/recipeapp/RecipesListFragment.kt
+++ app/src/main/java/com/example/recipeapp/RecipesListFragment.kt
@@ -39,7 +39,6 @@ class RecipesListFragment: Fragment() {
         return binding.root
     }
 
-    @SuppressLint("DiscouragedApi")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
