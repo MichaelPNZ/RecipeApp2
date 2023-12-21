b30dc64de3fefbf3525f482cb7027d2deb43407e
PozyalovMihail <pozyalov@gmail.com>
2023-12-22 00:46:25 +0300

    RA-18 Реализовал экран "Избранное"

--- app/src/main/java/com/example/recipeapp/FavoritesFragment.kt
+++ app/src/main/java/com/example/recipeapp/FavoritesFragment.kt
@@ -1,8 +1,6 @@
 package com.example.recipeapp
 
-import android.graphics.BitmapFactory
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,17 +8,14 @@ import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
 import androidx.fragment.app.replace
+import com.example.recipeapp.data.Recipe
 import com.example.recipeapp.databinding.FragmentFavoritesBinding
-import java.io.InputStream
 
 class FavoritesFragment: Fragment() {
 
     private lateinit var binding: FragmentFavoritesBinding
+    private var recipe: Recipe? = null
     private var recipesList = STUB
-
-    private var categoryId: Int? = null
-    private var categoryName: String? = null
-    private var categoryImageUrl: String? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -37,36 +32,31 @@ class FavoritesFragment: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        initUI()
         initRecycler()
-    }
-
-    private fun initUI() {
-        arguments?.let {
-            categoryId = it.getInt(ARG_CATEGORY_ID)
-            categoryName = it.getString(ARG_CATEGORY_NAME)
-            categoryImageUrl = it.getString(ARG_CATEGORY_IMAGE_URL)
-        }
-
-//        binding.tvHeaderRecipeCategory.text = categoryName
-
-        try {
-            val inputStream: InputStream? = context?.assets?.open(categoryImageUrl.toString())
-            val bitmap = BitmapFactory.decodeStream(inputStream)
-//            binding.ivHeaderRecipeCategory.setImageBitmap(bitmap)
-        } catch (ex: Exception) {
-            Log.e("mylog", "Error: ${ex.stackTraceToString()}")
-        }
     }
 
     private fun initRecycler() {
         val allPreferences = RecipeFragment().getFavorites()
-//        val recipesListAdapter = RecipesListAdapter()
-//        recipesListAdapter.setOnItemClickListener()
+        println(allPreferences)
+        val allKey = allPreferences.map { it.toInt() }.toSet()
+        val filteredRecipes = recipesList.getRecipesByIds(allKey)
+
+        val favoritesListAdapter = RecipesListAdapter(filteredRecipes, this)
+    favoritesListAdapter.setOnItemClickListener(object : RecipesListAdapter.OnItemClickListener {
+        override fun onItemClick(categoryId: Int) {
+            openRecipeByRecipeId(categoryId)
+        }
+    })
+        if (filteredRecipes.isEmpty()) {
+            binding.tvFavoritesFragment.visibility = View.VISIBLE
+        } else {
+            binding.tvFavoritesFragment.visibility = View.GONE
+        }
+        binding.rvFavorites.adapter = favoritesListAdapter
     }
 
-    private fun openRecipeByRecipeId(recipeId: Int) {
-        val recipe = recipesList.getRecipeById(recipeId)
+    private fun openRecipeByRecipeId(categoryId: Int) {
+        val recipe = recipesList.getRecipeById(categoryId)
 
         val bundle = bundleOf(ARG_RECIPE to recipe)
         parentFragmentManager.commit {
