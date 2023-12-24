690c139fc6e8d61db683d575d6eeed3abb79c1ef
PozyalovMihail <pozyalov@gmail.com>
2023-12-24 09:25:13 +0300

    RA-20 убрал лишние переменные

--- app/src/main/java/com/example/recipeapp/ui/recipes/recipe/RecipeViewModel.kt
+++ app/src/main/java/com/example/recipeapp/ui/recipes/recipe/RecipeViewModel.kt
@@ -1,19 +1,13 @@
 package com.example.recipeapp.ui.recipes.recipe
 
 import androidx.lifecycle.ViewModel
-import com.example.recipeapp.R
-import com.example.recipeapp.model.Ingredient
+import com.example.recipeapp.model.Recipe
 
 class RecipeViewModel : ViewModel() {
 
     data class RecipeUIState(
-        var recipeId: String? = null,
-        var recipeTitle: String? = null,
-        var recipeImageUrl: String? = null,
-        var ingredients: List<Ingredient>? = null,
-        var method: List<String>? = null,
-        val isHeart: Int = R.drawable.ic_heart,
-        val isHeartEmpty: Int = R.drawable.ic_heart_empty,
+        var recipe: Recipe? = null,
+        val isFavorite: Boolean? = null,
     )
 
 }
