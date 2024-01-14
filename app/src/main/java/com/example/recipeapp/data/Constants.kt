9e77b643fa523b1bb39403977d6313f31fab4945
mihailpozyalov <pozyalov@gmail.com>
2024-01-14 12:57:29 +0300

    RA-36 Загрузил и обработал изображения с помощью Glide

--- app/src/main/java/com/example/recipeapp/data/Constants.kt
+++ app/src/main/java/com/example/recipeapp/data/Constants.kt
@@ -1,9 +1,4 @@
 package com.example.recipeapp.data
 
-const val ARG_CATEGORY_ID = "arg_category_id"
-const val ARG_CATEGORY_NAME = "arg_category_name"
-const val ARG_CATEGORY_IMAGE_URL = "arg_category_image_url"
-const val ARG_RECIPE = "arg_recipe"
-const val ARG_RECIPE_ID = "arg_recipe_id"
 const val PREF_NAME = "my_settings"
 const val FAVORITES_KEY = "favoritesKey"
